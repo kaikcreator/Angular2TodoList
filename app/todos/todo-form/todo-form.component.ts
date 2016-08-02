e805112c0ee1bf23669db70afbfad901ba76adc5
kaikcreator <enriqueoriol@gmail.com>
2016-08-03 01:28:40 +0200

    added submit feature

--- app/todos/todo-form/todo-form.component.ts
+++ app/todos/todo-form/todo-form.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { NgForm }    from '@angular/forms';
 import { TodoModel }    from '../shared/todo.model';
+import { TodoService } from '../shared/todo.service';
 
 
 @Component({
   selector: 'todo-form',
-  templateUrl: 'app/todos/todo-form/todo-form.component.html'
+  templateUrl: 'app/todos/todo-form/todo-form.component.html',
+  providers:   [TodoService]
 })
 
 export class TodoFormComponent {
-  model = new TodoModel('new subject', 'new content', false, false);
+  model = new TodoModel('', 'new content', false, false);
   submitted = false;
-  onSubmit() { this.submitted = true; }
+  active = true;
+
+  constructor(private todoService:TodoService){}
+
+  newTodo(){
+    this.model = new TodoModel('', '', false, false);
+    this.active = false;
+    setTimeout(()=>{this.active = true;}, 0);
+  }
+  onSubmit() { 
+    this.submitted = true;
+    this.todoService.addTodo(this.model);
+    this.newTodo();
+  }
 }
